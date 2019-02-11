c2fed6fae8223a6398b1b62612e27dd1cd51cb15
Dimitar Nanov <dimitar.nanov@ecollect.net>
2019-02-11 08:43:50 +0200

    0.1.0-beta.0

--- index.js
+++ index.js
@@ -1,47 +1,69 @@
 // https://eslint.org/docs/user-guide/configuring
 module.exports = {
-	root: true,
-	parserOptions: {
-		parser: 'babel-eslint'
-	},
-    env: {
-        browser: true,
-        es6: true,
-        node: true
-	},
-	settings: {
-		"import/resolve": {
-			webpack: {
-				config: "build/webpack.base.conf.js"
-			}
-		}
-	},
-	// https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-	// consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-	extends: [
-		'ecollect-base',
-		'plugin:vue/recommended',
-	],
-	rules: {
-		"vue/html-indent": ["error", 'tab', {
-			"attribute": 1,
-			"closeBracket": 0,
-			"alignAttributesVertically": true,
-			"ignores": []
-		}],
-		"vue/script-indent": ["error", 'tab', {
-			"baseIndent": 1,
-			"switchCase": 0,
-			"ignores": []
-		}],
-		"vue/require-default-prop": 0
-	},
-	overrides: [
-		{
-			files: ['*.vue'],
-		  	rules: {
-				indent: 'off'
-		  	}
-		}
-	]
-}
+  parserOptions: {
+    parser: "babel-eslint",
+    sourceType: "module"
+  },
+  env: {
+    browser: true
+  },
+  extends: [
+    "plugin:vue/recommended",
+    requrie.resolve("./rules/imports.js"),
+    require.resolve("eslint-config-ecollect-base")
+  ],
+  plugins: ["vue"],
+  globals: {
+    ga: true, // Google Analytics
+    cordova: true,
+    __statics: true
+  },
+  settings: {
+    "import/resolve": {
+      webpack: {
+        config: "build/webpack.base.conf.js"
+      }
+    }
+  },
+  rules: {
+    "vue/html-indent": [
+      "error",
+      "tab",
+      {
+        attribute: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: []
+      }
+    ],
+    "vue/script-indent": [
+      "error",
+      "tab",
+      {
+        baseIndent: 1,
+        switchCase: 0,
+        ignores: []
+      }
+    ],
+    "vue/require-default-prop": 0,
+    "vue/html-self-closing": 0,
+    "vue/max-attributes-per-line": [
+      "error",
+      {
+        singleline: 3,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: ["*.vue"],
+      rules: {
+        indent: "off"
+      }
+    }
+  ]
+};
